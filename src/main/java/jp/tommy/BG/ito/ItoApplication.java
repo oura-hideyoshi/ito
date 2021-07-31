811bec68689bcb14a7ada074526d575121d82da1
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2021-07-31 23:25:32 +0900

    Initial commit

---
+++ src/main/java/jp/tommy/BG/ito/ItoApplication.java
@@ -0,0 +1,13 @@
+package jp.tommy.BG.ito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/tommy/BG/ito/ItoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.tommy.BG.ito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
